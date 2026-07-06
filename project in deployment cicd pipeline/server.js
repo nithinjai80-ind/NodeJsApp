b4e4ee7cb723021423dde402edb7d393385358a4
Nithin Jai <nithinjai80@gmail.com>
2026-07-06 11:01:28 +0530

    screenshort

---
+++ project in deployment cicd pipeline/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("<h1>Node.js Application Running Successfully 🚀</h1>");
+});
+
+app.get("/health", (req, res) => {
+    res.json({
+        status: "UP"
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
